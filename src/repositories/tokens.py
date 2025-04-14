12f3f054ffbc943a3eb946d8c3f0166b2eaaee5a
PentiukPavel <pentukpavel@yandex.ru>
2025-04-14 11:01:43 +0300

    ref: add docstrings for repositories

--- src/repositories/tokens.py
+++ src/repositories/tokens.py
@@ -15,7 +15,7 @@ class TokenRepository(AbstrsctSQLAlcchemyRepository):
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_active_token(self, user_id: int):
+    async def get_active_token(self, user_id: int) -> Token | None:
         """
         Получение активного токена по ID пользователя.
 
@@ -31,20 +31,36 @@ class TokenRepository(AbstrsctSQLAlcchemyRepository):
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
 
-    async def create_token(self, data: dict):
+    async def create_token(self, data: dict) -> Token:
         """
         Создание токена в БД.
+
+        :param data: данные токена
+        :return: токен
         """
 
         stmt = insert(Token).values(data).returning(Token)
         result = await self.session.execute(stmt)
         return result.scalar_one()
 
-    async def delete_token(self, token_id: int):
+    async def delete_token(self, token_id: int) -> None:
+        """
+        Удаление токена из БД.
+
+        :token_id data: ID токена
+        """
+
         stmt = delete(Token).where(Token.id == token_id)
         await self.session.execute(stmt)
 
-    async def get_active_token_by_email(self, email):
+    async def get_active_token_by_email(self, email) -> Token | None:
+        """
+        Получение активного токена по email пользователя из БД.
+
+        :param email: email пользователя
+        :return: Активный токен
+        """
+
         query = (
             select(Token)
             .join(User, Token.user_id == User.id)
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from sqlalchemy import and_, insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,10 +9,21 @@ from repositories.base import AbstrsctSQLAlcchemyRepository
 
 
 class UserRepository(AbstrsctSQLAlcchemyRepository):
+    """
+    SQLAlchemy репозиторий для CRUD операций с польщователями в БД.
+    """
+
     def __init__(self, session: AsyncSession):
         self.session = session
 
     async def get_user_by_code(self, code) -> User | None:
+        """
+        Получение пользователя по реферальному коду из БД.
+
+        :param code: реферальный код
+        :return: пользователь
+        """
+
         query = (
             select(User)
             .join(Token, Token.user_id == User.id)
@@ -26,12 +38,29 @@ class UserRepository(AbstrsctSQLAlcchemyRepository):
         return result.scalar_one_or_none()
 
     async def refer_to_user(self, user: User, referrer: User) -> None:
+        """
+        Создание записи о подписке на пользователя в БД.
+
+        :param user: реферал
+        :param referrer: референт
+        """
+
         stmt = insert(Referral).values(
             {"referral_id": user.id, "referrer_id": referrer.id}
         )
         await self.session.execute(stmt)
 
-    async def follow_up_check(self, user: User, referrer: User):
+    async def follow_up_check(
+        self, user: User, referrer: User
+    ) -> Referral | None:
+        """
+        Проверка наличия записи о подписке в БД.
+
+        :param user: реферал
+        :param referrer: референт
+        :return: подписка
+        """
+
         query = select(Referral).where(
             and_(
                 Referral.referral_id == user.id,
@@ -41,7 +70,14 @@ class UserRepository(AbstrsctSQLAlcchemyRepository):
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
 
-    async def get_referrers_of_user(self, user: User):
+    async def get_referrers_of_user(self, user: User) -> List[User]:
+        """
+        Получение списка рефералов пользователя из БД.
+
+        :param user: пользователь
+        :return список рефералов пользователя:
+        """
+
         query = user.referrers
         result = await self.session.execute(query)
         return result.scalars().all()
